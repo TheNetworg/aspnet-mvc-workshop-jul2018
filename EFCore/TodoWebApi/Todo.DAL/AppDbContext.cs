09f99ef95d485b5beebe7e45cda280d8334daf9e
Jan Skála <j.skala@hotmail.com>
2018-07-09 13:38:18 +0200

    Add migration.

--- EFCore/TodoWebApi/Todo.DAL/AppDbContext.cs
+++ EFCore/TodoWebApi/Todo.DAL/AppDbContext.cs
@@ -5,6 +5,13 @@ namespace Todo.DAL
 {
     public class AppDbContext : DbContext
     {
-       
+        public DbSet<TodoItem> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=TodoDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
     }
 }
