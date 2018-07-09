56e5439d212842c5b87df63d9dd6ff688b8e5128
Jan Skála <j.skala@hotmail.com>
2018-07-09 16:42:45 +0200

    Add brutal implementetion. thank you Jan Hajek

--- Auth/TodoWebApi/TodoWebApi/Startup.cs
+++ Auth/TodoWebApi/TodoWebApi/Startup.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 using Todo.BL;
 using Todo.DAL;
+using Todo.DAL.Entities;
 
 namespace TodoWebApi
 {
@@ -27,8 +33,21 @@ namespace TodoWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
+            services.AddDbContext<AppDbContext>();
+            services.AddScoped<ITodoService, TodoServiceEF>();
+            
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = $"https://login.microsoftonline.com/common/v2.0/";
+                    options.Audience = "1e2bec9a-1b43-4b5a-901a-cac3f0bb00c9";
+                    options.TokenValidationParameters.ValidateIssuer = true;
+                    options.TokenValidationParameters.IssuerValidator = ValidateIssuer;
+                });
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info
@@ -37,10 +56,54 @@ namespace TodoWebApi
                     Version = "v1",
                     Description = "Huge API for all todo services you can imagine.",
                 });
+                c.AddSecurityDefinition("OAuth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Flow = "implicit",
+                    AuthorizationUrl = $"https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+                    Scopes = new Dictionary<string, string>
+                    {
+                        {"openid", "openid" }
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme() { Description = "Your Bearer token obtained from Azure AD. Do not forget the correct format: Bearer <token>", In = "header", Name = "Authorization" });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    { "OAuth2", new string[] { } },
+                    { "Bearer", new string[] { } }
+                });
+
             });
 
-            services.AddDbContext<AppDbContext>();
-            services.AddScoped<ITodoService, TodoServiceEF>();            
+            services.AddMvc();
+        }
+
+        private string ValidateIssuer(string issuer, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            Uri uri = new Uri(issuer);
+            Uri authorityUri = new Uri("https://login.microsoftonline.com/");
+            string[] parts = uri.AbsolutePath.Split('/');
+            if (parts.Length >= 2)
+            {
+                Guid tenantId;
+                if (uri.Scheme != authorityUri.Scheme || uri.Authority != authorityUri.Authority)
+                {
+                    throw new SecurityTokenInvalidIssuerException("Issuer has wrong authority");
+                }
+                if (!Guid.TryParse(parts[1], out tenantId))
+                {
+                    throw new SecurityTokenInvalidIssuerException("Cannot find the tenant GUID for the issuer");
+                }
+                if (parts.Length > 2 && parts[2] != "v2.0")
+                {
+                    throw new SecurityTokenInvalidIssuerException("Only accepted protocol versions are AAD v1.0 or V2.0");
+                }
+                return issuer;
+            }
+            else
+            {
+                throw new SecurityTokenInvalidIssuerException("Unknown issuer");
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,7 +128,8 @@ namespace TodoWebApi
 
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TOOO API v1");                
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TOOO API v1");
+                c.OAuthClientId("1e2bec9a-1b43-4b5a-901a-cac3f0bb00c9");
             });
         }
     }
